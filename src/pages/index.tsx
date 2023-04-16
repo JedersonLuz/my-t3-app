a20b4b728c23b66f37ea203697284178ebfbfc2e
JedersonLuz <jedersonalpha@gmail.com>
2023-04-16 17:55:05 -0300

    feat: add login button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -43,7 +43,14 @@ const CreatePostWizard: React.FC = () => {
   const { data: sessionData } = useSession();
 
   if (!sessionData) {
-    return null;
+    return (
+      <button
+        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+        onClick={() => void signIn()}
+      >
+        Sign in
+      </button>
+    );
   }
 
   return (
