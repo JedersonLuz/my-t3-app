97187bb2d9339521af53d7977f0c56acfab082b7
JedersonLuz <jedersonalpha@gmail.com>
2023-04-16 17:44:41 -0300

    feat: author infos and profile pic added to posts

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,12 +1,39 @@
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
-import {
-  createTRPCRouter,
-  publicProcedure,
-} from "~/server/api/trpc";
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const postRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+      take: 100,
+    });
+
+    const users = await ctx.prisma.user.findMany({
+      where: {
+        id: {
+          in: posts.map((post) => post.authorId),
+        },
+      },
+      take: 100,
+    });
+
+    console.log(users);
+
+    return posts.map((post) => {
+      const author = users.find((user) => user.id === post.authorId);
+
+      if (!author) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Author for post not found",
+        });
+      }
+
+      return {
+        post,
+        author,
+      };
+    });
   }),
 });
