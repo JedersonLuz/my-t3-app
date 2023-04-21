5f292f6374cc10811f777922043654b77d09ab9d
JedersonLuz <jedersonalpha@gmail.com>
2023-04-21 14:02:00 -0300

    fit: head tag moved to app file to be rendered in all pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 import { Toaster } from "react-hot-toast";
+import Head from "next/head";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -13,6 +14,11 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <title>My Emoji Twitter</title>
+        <meta name="description" content="My Emoji Twitter" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Toaster position="bottom-center" />
       <Component {...pageProps} />
     </SessionProvider>
